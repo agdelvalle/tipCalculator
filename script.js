ff78249606f8ff6e19ed42097bf2823e7afd24e9
unknown <aly.delvalle.9@gmail.com>
2021-09-05 15:01:11 +0800

    finalize calculator functionality

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-var tipAmt; 
-
 var btn = document.querySelectorAll('.btn');
 var custom = document.getElementById('custom');
 var reset = document.getElementById('reset');
-// var person = document.getElementById('person');
 
 function tipUpdate(){
     var total = document.getElementById('bill').value;
@@ -11,37 +8,36 @@ function tipUpdate(){
     var tipPer = this.innerHTML;
     var tipPerB = this.value;
     var totalTip = total * (parseInt(tipPer)/100);
-    // var totalTipB = total * (parseInt(tipPerB)/100);
+    var totalTipB = total * (parseInt(tipPerB)/100);
     var perPerson = totalTip / (parseInt(people));
+    var perPersonB = totalTipB / (parseInt(people));
     var totalPerPerson = (total/parseInt(people))+perPerson;
-    // var perPersonB = totalTipB / (parseInt(people));
+    var totalPerPersonB = (total/parseInt(people))+perPersonB;
 
     if(people === "0"){
         alert("Number of people cannot be zero!");
+        resetFunc();
     } else {
-        console.log("tip:"+tipPer)
-        console.log("perPerson:"+perPerson)
-        console.log("total per person: "+totalPerPerson);
-        document.getElementById('finalTip').innerHTML = "$"+perPerson.toFixed(2);
-        document.getElementById('finalAmt').innerHTML = "$"+totalPerPerson.toFixed(2);
-    }
+        if(tipPerB === undefined){
+            document.getElementById('finalTip').innerHTML = "$"+perPerson.toFixed(2);
+            document.getElementById('finalAmt').innerHTML = "$"+totalPerPerson.toFixed(2);
+        } else {
+            document.getElementById('finalTip').innerHTML = "$"+perPersonB.toFixed(2);
+            document.getElementById('finalAmt').innerHTML = "$"+totalPerPersonB.toFixed(2);
+        };
+    };
 }
 
 function resetFunc(){
-    var totalC = document.getElementById('bill');
-    var peopleC = document.getElementById('people');
-    document.getElementById('finalTip').innerHTML = "$" + 0.00;
-    document.getElementById('finalAmt').innerHTML = "$" + 0.00
-    totalC.value = "0.00";
-    peopleC.value = "0";
+    var total = document.getElementById('bill');
+    var people = document.getElementById('people');
+    document.getElementById('finalTip').innerHTML = "$" + 0.00.toFixed(2);
+    document.getElementById('finalAmt').innerHTML = "$" + 0.00.toFixed(2)
+    total.value = "0.00";
+    people.value = "0";
     custom.value = "custom";
-    tipPerPerson.innerHTML = 0.00;
 }
 
 custom.addEventListener('change',tipUpdate);
-// people.addEventListener('change',tipUpdate);
-// document.getElementById('person').addEventListener('change', );
-
 reset.addEventListener('click',resetFunc);
-
 btn.forEach(num => num.addEventListener('click',tipUpdate));
